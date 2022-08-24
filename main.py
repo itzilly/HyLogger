150dcb6ec3e12caf44468ff02cb79aebd252eb40
itzilly <itzillyum@gmail.com>
2022-08-24 05:18:19 -0600

    Fix update check to be optional

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ def run():
         data = json.load(config_file)
     version = data.get('version')
     config_version = data.get('config_version')
+    check_update = data.get('check_update')
     print(f"Running HyLogger Version: '{version}'")
-    url = "https://api.github.com/repos/itzilly/HyLogger/contents/VERSION"
-    response = requests.get(url).json()
-    response_content = response.get('content')
-    content_bytes = response_content.encode('ascii')
-    latest_release_bytes = base64.b64decode(content_bytes)
-    content = latest_release_bytes.decode('ascii')
-    latest_release = content.split('=')[1]
-    if version != latest_release:
-        print(f"New release avalible! '{latest_release}'")
+    if check_update:
+        url = "https://api.github.com/repos/itzilly/HyLogger/contents/VERSION"
+        response = requests.get(url).json()
+        response_content = response.get('content')
+        content_bytes = response_content.encode('ascii')
+        latest_release_bytes = base64.b64decode(content_bytes)
+        content = latest_release_bytes.decode('ascii')
+        latest_release = content.split('=')[1]
+        if version != latest_release:
+            print(f"New release avalible! '{latest_release}'")
 
 if __name__ == "__main__":
     run()
